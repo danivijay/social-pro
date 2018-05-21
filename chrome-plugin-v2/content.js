c7ef7d8fc3d9c17294d58c0b8284c038610fbdeb
Dani Vijay <danimvijay@gmail.com>
2018-05-21 20:22:09 +0530

    strip out html tags from title, turn copy to clipboard a function

--- chrome-plugin-v2/content.js
+++ chrome-plugin-v2/content.js
@@ -5,20 +5,27 @@ setTimeout(function(){
     if (url.indexOf('?' !== -1)) {
       url = url.split("?")[0]
     }
-    var post = (document.getElementsByClassName('graf')[0].innerHTML).replace(/&nbsp;/g, ' ') + ' \n\n' + url
+    var post = socialProStripTags((document.getElementsByClassName('graf')[0].innerHTML).replace(/&nbsp/g, ' ')) + ' \n\n' + url
     console.log(post)
-    navigator.clipboard.writeText(post).then(function() {
-      console.log('Async: Copying to clipboard was successful!');
-    }, function(err) {
-      console.error('Async: Could not copy text: ', err);
-    });
+    socialProCopyToClipboard(post)
   } else if (url.indexOf('https://www.cssdesignawards.com/') !== -1) {
     var post = '#inspiration' + '\n\n' + document.getElementsByClassName("single-website__thumbnail")[0].href
     console.log(post)
-    navigator.clipboard.writeText(post).then(function() {
-      console.log('Async: Copying to clipboard was successful!');
-    }, function(err) {
-      console.error('Async: Could not copy text: ', err);
-    });
+    socialProCopyToClipboard(post)
   }
-}, 2000);
+}, 2000)
+
+function socialProCopyToClipboard(post) {
+  navigator.clipboard.writeText(post).then(function() {
+    console.log('Async: Copying to clipboard was successful!')
+  }, function(err) {
+    console.error('Async: Could not copy text: ', err)
+  })
+}
+
+function socialProStripTags(html)
+{
+   var tmp = document.createElement("DIV")
+   tmp.innerHTML = html
+   return tmp.textContent || tmp.innerText || ""
+}
